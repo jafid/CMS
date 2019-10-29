5f1e7542cc211c668046d488948388888a134940
Jafar <jafid8469@gmail.com>
2019-10-29 03:25:02 -0700

    Clinic System

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body style="background: url('12.jpg'); no-repeat; background-size: cover;">
+<div class="container" style="width: 400px; margin-top: 100px;">
+	<div class="card">
+		<img src="11.jpg" class="card-img-top">
+		<div class="card-body">
+	<form class="form-group" action="admin-panel.php" method="post">
+		<label>username :</label><br>
+		<input type="text" name="username" class="form-control" placeholder="enter username"><br>
+		<label>password :</label><br>
+		<input type="password" name="password" class="form-control" placeholder="enter password"><br>
+		<input type="submit" name="submit_login" class="btn btn-primary">
+		
+
+
+
+
+
+
+	</form>
+</div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
